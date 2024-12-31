2f905baf3b90079b143afdccaf101ed63f7c2e9b
singhshubham11 <kitesmaurya@gmail.com>
2024-12-31 12:01:01 +0000

    init

--- hooks_practise/02_use_effect/src/App.jsx
+++ hooks_practise/02_use_effect/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   useEffect(() => {
+//     document.title = `you have clicked ${count}`
+//   }, [count])
+
+//   return (
+//     <>
+//      <h1>you clicked {count} times </h1>
+//      <button onClick={() => setCount(count + 1)}>click</button>
+//     </>
+//   )
+// }
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [seconds, setSeconds] = useState(0);
 
-  return (
-    <>
-     <h1>HELLO</h1>
-    </>
-  )
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000); // Runs every second.
+
+    // Cleanup: Stop the timer when the component goes away.
+    return () => clearInterval(timer);
+  }, []); // Empty array = only run once at the beginning.
+
+  return <p>Timer: {seconds} seconds</p>;
 }
 
 export default App

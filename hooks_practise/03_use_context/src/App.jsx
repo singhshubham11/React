a983e31f2eb54b063387658a8ac919fbeeccc8b2
singhshubham11 <kitesmaurya@gmail.com>
2025-01-06 18:26:41 +0000

    init

--- hooks_practise/03_use_context/src/App.jsx
+++ hooks_practise/03_use_context/src/App.jsx
@@ -131,28 +131,118 @@ import { useState } from 'react'
 // }
 
 
+// const ThemeContext = createContext();
+
+// function App() {
+//   const theme = "dark";
+//   return(<>
+//   <ThemeContext.Provider value={theme}>
+//     <Header />
+//     <Footer />
+//   </ThemeContext.Provider>
+//   </>);
+// }
+
+// function Header() {
+//   const theme = useContext(ThemeContext);
+//   return <h1>The current theme is {theme}</h1>
+// }
+// function Footer() {
+//   const theme = useContext(ThemeContext);
+//   return <p>Footer also knows the theme: {theme}</p>;
+// }
+
+
+
+// const ThemeContext = createContext('light');
+
+// function App() {
+//   return(<>
+//   <ThemeContext.Provider value="dark">
+//     <Toolbar />
+//   </ThemeContext.Provider>
+//   </>);
+// }
+
+// function Toolbar() {
+//   return <Button />
+// }
+
+// function Button() {
+//   const theme = useContext(ThemeContext);
+//   return(
+//     <button style={{ background: theme === 'dark' ? 'black' : 'white', color: theme === 'dark' ? 'white' : 'black' }}>
+//       I am styled with the {theme} theme!
+//     </button>
+
+//   );
+// }
+
+
+
+// const ThemeContext = createContext("light");
+
+// function App() {
+//   return(
+//     <ThemeContext.Consumer>
+//       {(theme) => (
+//         <button
+//         style={{
+//           background: theme === 'dark' ? 'black' : 'white',
+//           color: theme === 'dark' ? 'white' : 'black',
+//         }}
+//         >
+//           I am Styled with the {theme} theme !
+//         </button>
+//       )}
+//     </ThemeContext.Consumer>
+//   );
+// }
+
+// function Button() {
+//   const theme = useContext(ThemeContext);
+
+//   return (
+//     <button
+//       style={{
+//         background: theme === 'dark' ? 'black' : 'white',
+//         color: theme === 'dark' ? 'white' : 'black',
+//       }}
+//     >
+//       I am styled with the {theme} theme!
+//     </button>
+//   );
+
+// }
+
+
+
 const ThemeContext = createContext();
 
 function App() {
-  const theme = "dark";
-  return(<>
-  <ThemeContext.Provider value={theme}>
-    <Header />
-    <Footer />
-  </ThemeContext.Provider>
-  </>);
+  const theme = 'light';
+  return(
+    <ThemeContext.Provider value={theme}>
+      <ThemedButoon />
+    </ThemeContext.Provider>
+  );
 }
 
-function Header() {
+function ThemedButoon() {
   const theme = useContext(ThemeContext);
-  return <h1>The current theme is {theme}</h1>
+  return (
+    <button
+      style={{
+        background: theme === 'dark' ? 'black' : 'white',
+        color: theme === 'dark' ? 'white' : 'black',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+      }}
+    >
+      {theme === 'dark' ? 'Dark Mode' : 'Light Mode'}
+    </button>
+  );
 }
-function Footer() {
-  const theme = useContext(ThemeContext);
-  return <p>Footer also knows the theme: {theme}</p>;
-}
-
-
-
 
 export default App
